2e78f335c6cd3a6965f55b7ca701e3a46ffc73b7
xgumball <anthonyviana4241997@gmail.com>
2015-11-13 01:48:25 +0000

    Done with medth argue

---
+++ lib/introduction.rb
@@ -0,0 +1,12 @@
+def introduction (name = "Josh") 
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language (name = "Josh" , language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction
+
+introduction_with_language
+
