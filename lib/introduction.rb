7cdef23a872b63795b89af4c7d6937c563228303
ashante <ashantejr@gmail.com>
2015-11-20 16:46:01 +0000

    ayyyyyye

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction(name = "Josh")
+  puts "Hi, my name is #{name}."
+end
+
+puts introduction
+
+def introduction_with_language(name = "Josh", language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
