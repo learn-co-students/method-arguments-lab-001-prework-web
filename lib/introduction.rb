246906be9595b7191b55a4881932ee3688184fa6
UrickBaptiste <urick_baptiste@hotmail.com>
2015-12-17 17:08:52 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,8 +2,14 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+introduction("Josh")
+
+
 def introduction_with_language(name, language)
-  "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language(Chris, Ruby)
+introduction_with_language("Dan", "Ember.js")
+
+
+
