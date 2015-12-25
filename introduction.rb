e7b3a27e0fd21569a6833627c68cbce169f68769
Bradley Allen <bradley.allen84@gmail.com>
2015-12-25 01:19:19 +0000

    implemented solution

--- introduction.rb
+++ introduction.rb
@@ -3,5 +3,7 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-	puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+	puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
+
+introduction("Brad")
