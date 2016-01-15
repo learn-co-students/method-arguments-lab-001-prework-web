d97e754d397c7aec421dc8a201380d5052e78dfc
orobert1 <orobert@risd.edu>
2016-01-15 13:19:58 -0500

    Done.

--- introduction_with_language.rb
+++ introduction_with_language.rb
@@ -1,6 +1,3 @@
-def introduction(string)
-  intro = puts("Hi, my name is #{string}.")
-end
 def introduction_with_language(name,lang)
   output = puts("Hi, my name is #{name} and I am learning to program in #{lang}.")
 end
