78196d36fabb415d47833271524ed0ccda5b770d
josephwilliams <josephwilli12@gmail.com>
2016-02-18 15:40:39 +0000

    nitrous-backup at Thu Feb 18 15:40:39 UTC 2016

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,3 +5,7 @@ end
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
+def say_hello(name = "Ruby Programmer")
+  puts "Hello #{name}"
+end
