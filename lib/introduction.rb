eeb6f0aab5f5884d1f5565401ea6609dc204c9d1
barbiecode <helesa@gmail.com>
2016-03-07 14:53:50 -0500

    Commit changes to lib

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+#introduction takes in an argument and uses that argument in the method body.
+def introduction(name)
+    puts "Hi, my name is #{name}."
+end
+
+#introduction_with_language takes in two arguments and uses both arguments in the method body
+def introduction_with_language(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
