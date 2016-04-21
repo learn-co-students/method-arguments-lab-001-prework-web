7e93851254834b5fd6a0033ea007020216001c0f
junklpm <lance.p.miller@gmail.com>
2016-04-20 21:04:34 -0400

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,7 @@ def introduction(name)
   end
 
   def introduction_with_language(name, language)
-    puts "Hi, my name is #{name} and I am learning to progrogram in #{language}."
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
  
