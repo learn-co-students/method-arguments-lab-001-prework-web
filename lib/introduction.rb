3979581bd7c5532716569c3f28dd271816dc12c4
iliass sabillah <iliass sabillah>
2016-05-25 17:51:59 -0400

    Methods and Arguments Lab

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,9 @@ def introduction(name)
 end
 
 introduction("iliass")
+
+def introduction_with_language(name, language)
+	puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language("iliass", "ruby")
