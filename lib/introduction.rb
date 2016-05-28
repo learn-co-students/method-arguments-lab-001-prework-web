4a1af2a0286b8c9c7bb513f7932e507a27eeea8c
SatomiBryant <satomi.bryant@gmail.com>
2016-05-28 01:09:02 -0400

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+def introduction(pipi)
+ puts "Hi, my name is #{pipi}.\n"
+end
+
+introduction("Satomi")
+
+def introduction_with_language(pipi,pecori)
+  puts "Hi, my name is #{pipi} and I am learning to program in #{pecori}.\n"
+
+  
+end
