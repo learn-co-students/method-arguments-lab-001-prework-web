4cae92d017d4a77badc405b5cee581982a5e6e5b
RKoopman <raoul.koopman@gmail.com>
2016-06-06 07:51:02 -0400

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(arg)
+  puts "Hi, my name is #{arg}."
+end
+
+def introduction_with_language(arg1, arg2)
+  puts "Hi, my name is #{arg1} and I am learning to program in #{arg2}."
+end
