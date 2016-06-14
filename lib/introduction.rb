aaf252f0e32187aaa060c1f7700a427d52f75340
Beth Olmo <betholmo13@gmail.com>
2016-06-14 19:51:02 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,14 @@
+# introduction.rb
+
+def introduction(name)
+  name = "Josh"
+  puts "Hi, my name is #{name}."
+end
+
+# introduction_with_language.rb
+
+def introduction_with_language(name, language)
+  name = "Dan"
+  language = "Ember.js"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
